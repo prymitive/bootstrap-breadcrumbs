9fdac2a6d3bd171f897b3f09d121ea283751bed9
Łukasz Mierzwa <lukasz.mierzwa@redefine.pl>
2012-10-02 11:44:42 +0200

    first version

--- README.md
+++ README.md
@@ -2,3 +2,41 @@ bootstrap-breadcrumbs
 =====================
 
 Django template tags used to generate breadcrumbs html using twitter bootstrap css classes.
+
+Requirements:
+
+  * Twitter Bootstrap 2 (written with 2.1)
+
+Example
+=======
+
+Add "django_bootstrap_breadcrumbs" to INSTALLED_APPS.
+
+base.html:
+
+  {% django_bootstrap_breadcrumbs %}
+
+  {% block breadcrumbs %}
+      {% breadcrumb "Home" "site_index" %}
+  {% endblock %}
+
+  {% block content %}
+      {% render_breadcrumbs %}
+  {% endblock %}
+
+users.html:
+
+  {% django_bootstrap_breadcrumbs %}
+
+  {% block breadcrumbs %}
+      {{ block.super }}
+      {% breadcrumb "Users" "users.views.index" %}
+  {% endblock %}
+
+profile.html
+  {% django_bootstrap_breadcrumbs %}
+
+  {% block breadcrumbs %}
+      {{ block.super }}
+      {% breadcrumb "User profile" "users.views.profile" user.username %}
+  {% endblock %}
---
+++ django_bootstrap_breadcrumbs/templatetags/django_bootstrap_breadcrumbs.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+    :copyright: Copyright 2012 by Łukasz Mierzwa
+    :contact: l.mierzwa@gmail.com
+"""
+
+
+from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext as _
+from django import template
+
+
+register = template.Library()
+
+
+CONTEXT_KEY = 'DJANGO_BREADCRUMB_LINKS'
+
+
+def breadcrumb(context, label, viewname, *args):
+    """
+    Add link to list of breadcrumbs, usage:
+
+    {% load bubbles_breadcrumbs %}
+    {% breadcrumb "Home" "index" %}
+
+    Remember to use it inside {% block %} with {{ block.super }} to get all
+    parent breadcrumbs.
+
+    :param label: Breadcrumb link label
+    :param viewname: Name of the view to link this breadcrumb to.
+    :param args: Any arguments to view function.
+    """
+    context['request'].META[CONTEXT_KEY] = context['request'].META.get(
+        CONTEXT_KEY, []) + [(label, viewname, args)]
+    return ''
+
+
+def render_breadcrumbs(context):
+    """
+    Render breadcrumbs html using twitter bootstrap css classes.
+    """
+    links = []
+    for (label, viewname, args) in context['request'].META.get(
+        CONTEXT_KEY, []):
+        url = reverse(viewname=viewname, args=args)
+        links.append((url, _(label)))
+
+    if not links:
+        return ''
+
+    ret = '<ul class="breadcrumb">'
+    total = len(links)
+    i = 1
+    for (url, label) in links:
+        ret += '<li>'
+        if total > 1 and i < total:
+            ret += '<a href="%s">%s</a>' % (url, label)
+            ret += ' <span class="divider">/</span>'
+        else:
+            ret += label
+        i += 1
+    ret += '</ul>'
+    return mark_safe(ret)
+
+
+register.simple_tag(takes_context=True)(breadcrumb)
+register.simple_tag(takes_context=True)(render_breadcrumbs)
