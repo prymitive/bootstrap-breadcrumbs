a61831057ed2b37dc0dfe1d28e71d8b8e9d10521
Łukasz Mierzwa <l.mierzwa@gmail.com>
2014-01-06 20:09:33 +0100

    switch to MIT license, fixes #11

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_bootstrap_breadcrumbs',
-    version='0.6.1',
+    version='0.6.2',
     url='http://prymitive.github.com/bootstrap-breadcrumbs',
-    license='GPLv3',
+    license='MIT',
     description='Django breadcrumbs using Twitter Bootstrap V2 or V3',
     long_description='Django template tags used to generate breadcrumbs html '
                      'using twitter bootstrap css classes or custom template',
@@ -21,7 +21,7 @@ setup(
     author_email='l.mierzwa@gmail.com',
     packages=find_packages(),
     classifiers=[
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
