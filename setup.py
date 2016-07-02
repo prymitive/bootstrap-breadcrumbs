89ebec2bc8ab2fe63ffc7a01aeb4b8491a501c31
Łukasz Mierzwa <l.mierzwa@gmail.com>
2016-07-02 08:43:44 -0700

    Release 0.8 with zopieux changes

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_bootstrap_breadcrumbs',
-    version='0.7.3',
+    version='0.8',
     url='http://prymitive.github.com/bootstrap-breadcrumbs',
     license='MIT',
     description='Django breadcrumbs for Bootstrap 2, 3 or 4',
