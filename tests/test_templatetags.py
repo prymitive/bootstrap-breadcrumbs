4900c81af305c67440da965b51eeef2fa3cb57e9
Łukasz Mierzwa <l.mierzwa@gmail.com>
2016-08-01 23:58:56 -0700

    use full module path

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -206,7 +206,7 @@ class Actor(Model):
     INSTALLED_APPS=['django', 'django_bootstrap_breadcrumbs', 'tests'],
     TEMPLATES=[{'BACKEND': 'django.template.backends.django.DjangoTemplates',
                 'APP_DIRS': True}],
-    ROOT_URLCONF='urls'
+    ROOT_URLCONF='tests.urls'
     )
 class SiteTests(TestCase):
 
