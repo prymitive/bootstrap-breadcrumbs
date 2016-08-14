95cb5536e71e566b3e5fef3b98a5016604e5e4d6
Łukasz Mierzwa <l.mierzwa@gmail.com>
2016-08-13 23:16:06 -0700

    fix tests

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -205,8 +205,7 @@ class Actor(Model):
 @override_settings(
     INSTALLED_APPS=['django', 'django_bootstrap_breadcrumbs', 'tests'],
     TEMPLATES=[{'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                'APP_DIRS': True}],
-    ROOT_URLCONF='tests.urls'
+                'APP_DIRS': True}]
     )
 class SiteTests(TestCase):
 
--- tests/urls.py
+++ tests/urls.py
@@ -7,21 +7,20 @@
 
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
+import django.contrib.auth.views
 
 
-nsurlpatters = patterns(
-    '',
-    url(r'^login2$', 'django.contrib.auth.views.login', name='login2_url'),
-)
+nsurlpatters = [
+    url(r'^login2$', django.contrib.auth.views.login, name='login2_url'),
+]
 
 
-urlpatterns = patterns(
-    '',
-    url(r'^login$', 'django.contrib.auth.views.login', name='login_url'),
-    url(r'^login/(?P<slug>[-_\w]+)$', 'django.contrib.auth.views.login',
+urlpatterns = [
+    url(r'^login$', django.contrib.auth.views.login, name='login_url'),
+    url(r'^login/(?P<slug>[-_\w]+)$', django.contrib.auth.views.login,
         name='login_args_url'),
-    url(r'^login/user/(?P<user_id>\S+)$', 'django.contrib.auth.views.login',
+    url(r'^login/user/(?P<user_id>\S+)$', django.contrib.auth.views.login,
         name='login_kwargs_url'),
     (r'^ns/', include(nsurlpatters, namespace='ns')),
-)
+]
