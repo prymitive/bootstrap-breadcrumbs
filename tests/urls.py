0369868085a47a9c01b7c7af2e611817df31e279
Łukasz Mierzwa <l.mierzwa@gmail.com>
2016-08-13 23:54:43 -0700

    one more try

--- tests/urls.py
+++ tests/urls.py
@@ -7,20 +7,37 @@
 
 from __future__ import unicode_literals
 
-from django.conf.urls import url, include
 import django.contrib.auth.views
+from django.conf.urls import url, include
+from django import VERSION
 
 
-nsurlpatters = [
-    url(r'^login2$', django.contrib.auth.views.login, name='login2_url'),
-]
+if VERSION < (1, 8):  # pragma: nocover
+    from django import patterns
+    nsurlpatters = patterns(
+        '',
+        url(r'^login2$', 'django.contrib.auth.views.login', name='login2_url'),
+    )
 
+    urlpatterns = patterns(
+        '',
+        url(r'^login$', 'django.contrib.auth.views.login', name='login_url'),
+        url(r'^login/(?P<slug>[-_\w]+)$', 'django.contrib.auth.views.login',
+            name='login_args_url'),
+        url(r'^login/user/(?P<user_id>\S+)$', 'django.contrib.auth.views.login',
+            name='login_kwargs_url'),
+        (r'^ns/', include(nsurlpatters, namespace='ns')),
+    )
+else:
+    nsurlpatters = [
+        url(r'^login2$', django.contrib.auth.views.login, name='login2_url'),
+    ]
 
-urlpatterns = [
-    url(r'^login$', django.contrib.auth.views.login, name='login_url'),
-    url(r'^login/(?P<slug>[-_\w]+)$', django.contrib.auth.views.login,
-        name='login_args_url'),
-    url(r'^login/user/(?P<user_id>\S+)$', django.contrib.auth.views.login,
-        name='login_kwargs_url'),
-    (r'^ns/', include(nsurlpatters, namespace='ns')),
-]
+    urlpatterns = [
+        url(r'^login$', django.contrib.auth.views.login, name='login_url'),
+        url(r'^login/(?P<slug>[-_\w]+)$', django.contrib.auth.views.login,
+            name='login_args_url'),
+        url(r'^login/user/(?P<user_id>\S+)$', django.contrib.auth.views.login,
+            name='login_kwargs_url'),
+        url(r'^ns/', include(nsurlpatters, namespace='ns')),
+    ]
