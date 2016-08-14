6ff5563bf1918bfa66d857a4377ec3b588423557
Łukasz Mierzwa <l.mierzwa@gmail.com>
2016-08-14 00:01:09 -0700

    pep8 fixes

--- tests/urls.py
+++ tests/urls.py
@@ -24,7 +24,8 @@ if VERSION < (1, 8):  # pragma: nocover
         url(r'^login$', 'django.contrib.auth.views.login', name='login_url'),
         url(r'^login/(?P<slug>[-_\w]+)$', 'django.contrib.auth.views.login',
             name='login_args_url'),
-        url(r'^login/user/(?P<user_id>\S+)$', 'django.contrib.auth.views.login',
+        url(r'^login/user/(?P<user_id>\S+)$',
+            'django.contrib.auth.views.login',
             name='login_kwargs_url'),
         (r'^ns/', include(nsurlpatters, namespace='ns')),
     )
