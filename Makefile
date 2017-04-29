d28089509f2fdc89ce63b791bf3a5d5f71d79215
Łukasz Mierzwa <l.mierzwa@gmail.com>
2017-04-29 01:02:41 -0700

    Add django 1.11 and python 3.6 to tested versions

--- Makefile
+++ Makefile
@@ -3,5 +3,5 @@
 	@docker build -q --build-arg DJANGO===$@ .
 	@echo Done
 
-all: 1.5.12 1.6.11 1.7.11 1.8.15 1.9.10 1.10.2
+all: 1.5.12 1.6.11 1.7.11 1.8.15 1.9.10 1.10.2 1.11
 	@echo All tests completed
--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -203,7 +203,8 @@ class Actor(Model):
 
 
 @override_settings(
-    INSTALLED_APPS=['django', 'django_bootstrap_breadcrumbs', 'tests'],
+    INSTALLED_APPS=['django', 'django.contrib.auth',
+                    'django_bootstrap_breadcrumbs', 'tests'],
     TEMPLATES=[{'BACKEND': 'django.template.backends.django.DjangoTemplates',
                 'APP_DIRS': True}]
     )
